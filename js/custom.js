e62dfa76b6f8257801d0e5c861cc55a4d6cd7e09
canhphan10994 <50878552+canhphan10994@users.noreply.github.com>
2019-05-23 17:31:52 +0700

    Add files via upload
    Phottos index page

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// IDEA: Navbar
+const toggle = document.querySelector(".toggle");
+const headNav = document.querySelector("#header-nav");
+
+toggle.addEventListener("click", () => {headNav.classList.toggle("hidden");})
